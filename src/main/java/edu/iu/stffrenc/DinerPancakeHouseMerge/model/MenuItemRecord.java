0aae811db218b908e173446eb6b46fd916e7a74f
French II, Stephan Floyd <stffrenc@iu.edu>
2024-04-03 17:35:44 -0400

    No More Backend Iterator Bugs

--- src/main/java/edu/iu/stffrenc/DinerPancakeHouseMerge/model/MenuItemRecord.java
+++
@@ -1,7 +0,0 @@
-package edu.iu.stffrenc.DinerPancakeHouseMerge.model;
-
-public record MenuItemRecord(String name,
-                             String description,
-                             boolean vegetarian,
-                             double price) {
-}
